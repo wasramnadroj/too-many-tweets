4836b2343251aae9baa9354afcbc9acc04d2b19e
wasramnadroj <wasramnadroj@gmail.com>
2017-06-08 22:49:52 -0500

    Create server.py
    still need to test

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, current_app
+from flask_script import Manager, Bootstrap #Bootstrap isn't really needed I guess cause of React but I might need later for Jinja
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 BUILD_PATH = os.path.join(ROOT_DIR, 'build/')
 
 app = Flask(__name__, static_folder='build/', static_url_path=BUILD_PATH, root_path=BUILD_PATH)
+manager = Manager(app)
+#bootstrap = Bootstrap(app)
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return current_app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
